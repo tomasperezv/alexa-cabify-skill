4aaad8a4f7b2c8ae9bf211830871589cac233aca
Alejandro rodriguez <hi@aro.io>
2017-02-09 10:25:27 +0100

    First response from Alexa when the user invokes the skill without specifying what they want

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 'use strict';
 
+// Allow this module to be reloaded by hotswap when changed
+module.change_code = 1;
+
 var Alexa = require('alexa-app');
 var app = new Alexa.app('cabify');
+
+/**
+ * Called when the user invokes the skill without specifying what they want.
+ */
+app.launch(function(req, res) {
+  res.say('Hello Cabify user!!');
+});
 
 module.exports = app;
